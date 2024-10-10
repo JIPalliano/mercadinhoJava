c8052f7c0ef63518cbda00fa7a6c2c321f3e9bac
JIPalliano <ismael_palliano190@hotmail.com>
2024-10-10 09:39:33 -0300

    Criando Cookie

--- src/main/java/com/example/mercadinho/controller/ProductController.java
+++ src/main/java/com/example/mercadinho/controller/ProductController.java
@@ -1,15 +1,17 @@
 package com.example.mercadinho.controller;
 
+import ch.qos.logback.core.model.Model;
 import com.example.mercadinho.model.ProductEntity;
 import com.example.mercadinho.model.ShoppingCartEntity;
 import com.example.mercadinho.repository.ProductRepository;
 import com.example.mercadinho.repository.ShoppingCartRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @AllArgsConstructor
@@ -18,6 +20,7 @@ public class ProductController {
 
     final ProductRepository repository;
     final ShoppingCartRepository shoppingCart;
+
 
     @PostMapping
     public ProductEntity createProduct(@RequestBody ProductEntity request) {
@@ -38,12 +41,27 @@ public class ProductController {
     public ShoppingCartEntity createShoppingcart(@PathVariable("id") String id, @RequestBody ShoppingCartEntity request) {
         var object = ShoppingCartEntity.builder().id(request.id()).products(request.products()).build();
         repository.findById(id).ifPresent(shoppingCart -> request.products().add(shoppingCart));
-        return object;
+        return shoppingCart.save(object);
+    }
+
+    @GetMapping(path="/shopping-cart/{id-product}/{id-shopping-cart}")
+    public ShoppingCartEntity findShoppingCartAdd(@PathVariable("id-product") String idProduct, @PathVariable("id-shopping-cart") String idShoppingCart, @RequestBody ShoppingCartEntity request) {
+        var object = ShoppingCartEntity.builder().id(idShoppingCart).products(request.products()).build();
+        repository.findById(idProduct).ifPresent(shoppingCart -> request.products().add(shoppingCart));
+        return shoppingCart.save(object);
     }
 
     @GetMapping(path="/shopping-cart/")
     public List<ShoppingCartEntity> findAllShoppingcart() {
         return this.shoppingCart.findAll();
+    }
+
+    @GetMapping(path="/cookie")
+    public Cookie createCookie(Model model, HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie = new Cookie("cookie-e-bom-nada", "cookie-value");
+        cookie.setMaxAge(3600);
+        response.addCookie(cookie);
+        return cookie;
     }
 
 }
