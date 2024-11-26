55117fdf46d61c7e401efc4fa07ee3f4bd07a9d6
JIPalliano <ismael_palliano190@hotmail.com>
2024-11-26 15:57:38 -0300

    testes de integração, fazendoa algumas alterações

--- src/test/java/com/example/mercadinho/controller/user/ShoppingCartControllerTest.java
+++ src/test/java/com/example/mercadinho/controller/user/ShoppingCartControllerTest.java
@@ -2,47 +2,53 @@ package com.example.mercadinho.controller.user;
 
 import com.example.mercadinho.domain.repository.ShoppingCartRepository;
 import com.example.mercadinho.domain.repository.model.Product;
+import com.example.mercadinho.domain.repository.model.entity.ProductEntity;
 import com.example.mercadinho.domain.repository.model.entity.ShoppingCartEntity;
 import com.example.mercadinho.domain.repository.model.entity.UserEntity;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.example.mercadinho.service.product.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
+@ExtendWith(SpringExtension.class)
 class ShoppingCartControllerTest {
 
-    @Mock
+    @Autowired
     private MockMvc mockMvc;
 
-    @Mock
-    private ObjectMapper objectMapper;
-
-    @Mock
+    @Autowired
     private ShoppingCartRepository shoppingCartRepository;
 
-    @Mock
     private UserEntity user;
+
+//    @MockBean
+//    private ProductService productService;
 
     @BeforeEach
     void setupUserService() {
+        shoppingCartRepository.deleteAll();
+
         user = UserEntity.builder()
                 .id("user1")
                 .username("user1")
@@ -58,38 +64,42 @@ class ShoppingCartControllerTest {
 
     @Test
     void testAddProductToShoppingCart() throws Exception {
-        // Prepara os dados para o teste
-        String idProduct = "12345";
+
+        ProductEntity productA = ProductEntity.builder()
+                .id("product1")
+                .name("Product 1")
+                .price(BigDecimal.valueOf(10.0))
+                .quantity(2)
+                .build();
         int quantity = 2;
 
-        // Executa a chamada PUT
-        mockMvc.perform(put("/shopping-cart/{id-product}", idProduct)
+        //when(productService.findById(productA)).thenReturn(productA);
+
+        mockMvc.perform(put("/shopping-cart/{id-product}", productA.id())
                         .param("quantity", String.valueOf(quantity))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
-        // Verifica se o produto foi adicionado ao carrinho
-        var shoppingCart = shoppingCartRepository.findByUserId(user.getId()).orElseThrow();
+        ShoppingCartEntity shoppingCart = shoppingCartRepository.findByUserId(user.getId()).orElseThrow();
 
         assertThat(shoppingCart.getProducts()).anyMatch(p ->
-                p.getId().equals(idProduct) && p.getQuantity() == quantity
+                p.getId().equals(productA.id()) && p.getQuantity() == quantity
         );
     }
 
     @Test
     void testFindShoppingCart() throws Exception {
-        // Prepara um carrinho no banco
+
         ShoppingCartEntity cart = new ShoppingCartEntity();
         cart.setUserId(user.getId());
-        cart.setProducts(List.of(Product.builder()
-                .id("product1")
+        cart.setProducts(new ArrayList<>(List.of(Product.builder()
+                .id("product2")
                 .name("Product 1")
                 .price(BigDecimal.valueOf(10.0))
-                .quantity(1)
-                .build()));
+                .quantity(2)
+                .build())));
         shoppingCartRepository.save(cart);
 
-        // Executa a chamada GET
         mockMvc.perform(get("/shopping-cart/user-shopping-cart")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -100,17 +110,15 @@ class ShoppingCartControllerTest {
 
     @Test
     void testDeleteShoppingCart() throws Exception {
-        // Prepara um carrinho no banco
+
         ShoppingCartEntity cart = new ShoppingCartEntity();
         cart.setUserId(user.getId());
         shoppingCartRepository.save(cart);
 
-        // Executa a chamada DELETE
         mockMvc.perform(delete("/shopping-cart/delete")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
 
-        // Verifica se o carrinho foi deletado
         assertThat(shoppingCartRepository.findByUserId(user.getId())).isEmpty();
     }
 
