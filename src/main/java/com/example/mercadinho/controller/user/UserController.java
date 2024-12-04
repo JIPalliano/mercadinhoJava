7a4ef5ec20a2727339a94dbb66fa1f4c61c3f5ba
JIPalliano <ismael_palliano190@hotmail.com>
2024-12-04 13:20:18 -0300

    comentando API do user

--- src/main/java/com/example/mercadinho/controller/user/UserController.java
+++ src/main/java/com/example/mercadinho/controller/user/UserController.java
@@ -2,10 +2,16 @@ package com.example.mercadinho.controller.user;
 
 import com.example.mercadinho.controller.request.LoginRequest;
 import com.example.mercadinho.controller.request.UserRequest;
+import com.example.mercadinho.controller.response.ErrorResponse;
 import com.example.mercadinho.controller.response.LoginResponse;
 import com.example.mercadinho.controller.response.UserResponse;
+import com.example.mercadinho.domain.repository.model.entity.ShoppingCartEntity;
 import com.example.mercadinho.service.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,6 +29,22 @@ public class UserController {
 
     @PostMapping("login")
     @Operation(summary = "Cria um carrinho de compras", description = "Cria o carrinho de compras a partir do produto adicionado no carrinho.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login efetuado com sucesso.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ShoppingCartEntity.class))}),
+            @ApiResponse(responseCode = "400", description = "Erro no login.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "Login não encontrado.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))}),
+            @ApiResponse(responseCode = "403", description = "Login não efetuado.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "Login não encontrado no banco.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))})})
     public LoginResponse loginUser(@RequestBody LoginRequest loginRequest) {
         return userService.loginUser(loginRequest);
     }
@@ -30,6 +52,22 @@ public class UserController {
     @PostMapping("register")
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "Cria um carrinho de compras", description = "Cria o carrinho de compras a partir do produto adicionado no carrinho.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Registro efetuado com sucesso.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ShoppingCartEntity.class))}),
+            @ApiResponse(responseCode = "400", description = "Erro no registro.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))}),
+            @ApiResponse(responseCode = "404", description = "Não foi possivel registrar o usuário.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))}),
+            @ApiResponse(responseCode = "403", description = "Login não efetuado.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))}),
+            @ApiResponse(responseCode = "500", description = "Registro não efetuado no banco.",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))})})
     public UserResponse registerUser(@RequestBody UserRequest request){
         return userService.registerUser(request);
     }
